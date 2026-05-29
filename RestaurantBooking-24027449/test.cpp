7ec5da5cbb82fdc4afe2500cda6e53703f70272c
donghyun.yoon <ydh1305@naver.com>
2026-05-29 13:56:10 +0900

    [refactoring] Add the BookingItem Fixture.

--- RestaurantBooking-24027449/test.cpp
+++ RestaurantBooking-24027449/test.cpp
@@ -1,17 +1,27 @@
 #include "gmock/gmock.h"
 #include "booking_scheduler.cpp"
 
-TEST(BookingSchedulerTest, 예약은정시에만가능하다정시가아닌경우예약불가) {
-	tm notOnTheHour = { 0 };
-	notOnTheHour.tm_year = 2021 - 1900;
-	notOnTheHour.tm_mon = 03 - 1;
-	notOnTheHour.tm_mday = 26;
-	notOnTheHour.tm_hour = 9;
-	notOnTheHour.tm_min = 5;
-	notOnTheHour.tm_isdst = -1;
-	mktime(&notOnTheHour);
+using namespace testing;
 
+class BookingItem : public Test {
+protected:
+	void SetUp() override {
+		notOnTheHour = getTime(2021, 3, 26, 9, 5);
+		onTheHour = getTime(2021, 3, 26, 9, 0);
+	}
+public:
+	tm getTime(int year, int mon, int day, int hour, int min) {
+		tm result = { 0, min, hour, day, mon - 1, year - 1900, 0, 0, -1 };
+		mktime(&result);
+		return result;
+	}
+
+	tm notOnTheHour;
+	tm onTheHour;
 	Customer customer{ "Fake name", "010-1234-5678" };
+};
+
+TEST_F(BookingItem, 예약은정시에만가능하다정시가아닌경우예약불가) {
 	Schedule* schedule = new Schedule{ notOnTheHour, 1, customer };
 	BookingScheduler bookingScheduler{ 3 };
 
@@ -20,18 +30,8 @@ TEST(BookingSchedulerTest, 예약은정시에만가능하다정시가아닌경우예약불가) {
 		}, std::runtime_error);
 }
 
-TEST(BookingSchedulerTest, 예약은정시에만가능하다정시인경우예약가능) {
-	tm OnTheHour = { 0 };
-	OnTheHour.tm_year = 2021 - 1900;
-	OnTheHour.tm_mon = 03 - 1;
-	OnTheHour.tm_mday = 26;
-	OnTheHour.tm_hour = 9;
-	OnTheHour.tm_min = 0;
-	OnTheHour.tm_isdst = -1;
-	mktime(&OnTheHour);
-
-	Customer customer{ "Fake name", "010-1234-5678" };
-	Schedule* schedule = new Schedule{ OnTheHour, 1, customer };
+TEST_F(BookingItem, 예약은정시에만가능하다정시인경우예약가능) {
+	Schedule* schedule = new Schedule{ onTheHour, 1, customer };
 	BookingScheduler bookingScheduler{ 3 };
 
 	bookingScheduler.addSchedule(schedule);
@@ -39,31 +39,31 @@ TEST(BookingSchedulerTest, 예약은정시에만가능하다정시인경우예약가능) {
 	EXPECT_EQ(true, bookingScheduler.hasSchedule(schedule));
 }
 
-TEST(BookingSchedulerTest, 시간대별인원제한이있다같은시간대에Capacity초과할경우예외발생) {
+TEST_F(BookingItem, 시간대별인원제한이있다같은시간대에Capacity초과할경우예외발생) {
 
 }
 
-TEST(BookingSchedulerTest, 시간대별인원제한이있다같은시간대가다르면Capacity차있어도스케쥴추가성공) {
+TEST_F(BookingItem, 시간대별인원제한이있다같은시간대가다르면Capacity차있어도스케쥴추가성공) {
 
 }
 
-TEST(BookingSchedulerTest, 예약완료시SMS는무조건발송) {
+TEST_F(BookingItem, 예약완료시SMS는무조건발송) {
 
 }
 
-TEST(BookingSchedulerTest, 이메일이없는경우에는이메일미발송) {
+TEST_F(BookingItem, 이메일이없는경우에는이메일미발송) {
 
 }
 
-TEST(BookingSchedulerTest, 이메일이있는경우에는이메일발송) {
+TEST_F(BookingItem, 이메일이있는경우에는이메일발송) {
 
 }
 
-TEST(BookingSchedulerTest, 현재날짜가일요일인경우예약불가예외처리) {
+TEST_F(BookingItem, 현재날짜가일요일인경우예약불가예외처리) {
 
 }
 
-TEST(BookingSchedulerTest, 현재날짜가일요일이아닌경우예약가능) {
+TEST_F(BookingItem, 현재날짜가일요일이아닌경우예약가능) {
 
 }
 
