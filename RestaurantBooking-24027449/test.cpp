88fa508befa4d2b61b81c6c299e56fc2ade13e2e
donghyun.yoon <ydh1305@naver.com>
2026-05-29 15:09:50 +0900

    [feature] 일요일 Mocking을 사용하는 테스트 케이스 - Test #8

--- RestaurantBooking-24027449/test.cpp
+++ RestaurantBooking-24027449/test.cpp
@@ -2,6 +2,8 @@
 #include "booking_scheduler.cpp"
 #include "testable_sms_sender.cpp"
 #include "testable_mail_sender.cpp"
+#include "sunday_booking_scheduler.cpp"
+#include "monday_booking_scheduler.cpp"
 
 using namespace testing;
 
@@ -105,7 +107,16 @@ TEST_F(BookingItem, 이메일이있는경우에는이메일발송) {
 }
 
 TEST_F(BookingItem, 현재날짜가일요일인경우예약불가예외처리) {
+	BookingScheduler* bookingScheduler = new SundayBookingScheduler{ CAPACITY_PER_HOUR };
 
+	try {
+		Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER_WITH_MAIL };
+		bookingScheduler->addSchedule(schedule);
+		FAIL();
+	}
+	catch (std::runtime_error& e) {
+		EXPECT_EQ(string{ e.what() }, string{ "Booking system is not available on sunday" });
+	}
 }
 
 TEST_F(BookingItem, 현재날짜가일요일이아닌경우예약가능) {
