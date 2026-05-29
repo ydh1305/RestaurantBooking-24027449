dc7919b8f26c7efb8f2c58f70c213a957d645c54
donghyun.yoon <ydh1305@naver.com>
2026-05-29 14:35:17 +0900

    [feature] SMS Test Double 만들기

---
+++ RestaurantBooking-24027449/testable_sms_sender.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "sms_sender.cpp"
+
+class TestableSmsSender : public SmsSender {
+public:
+	void send(Schedule* schedule) override {
+		std::cout << "테스트용 SmsSender class의 send메서드 실행됨\n";
+		sendMethodIsCalled = true;
+	}
+
+	bool isSendMethodIsCalled() {
+		return sendMethodIsCalled;
+	}
+private:
+	bool sendMethodIsCalled;
+};
