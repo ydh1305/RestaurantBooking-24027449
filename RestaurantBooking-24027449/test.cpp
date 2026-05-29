4ecff1b345e9a39eb7ea972fa2ce15d4d65f44e2
donghyun.yoon <ydh1305@naver.com>
2026-05-29 14:40:36 +0900

    [refactoring] testableSmsSender를 Fixture로 옮기기

--- RestaurantBooking-24027449/test.cpp
+++ RestaurantBooking-24027449/test.cpp
@@ -9,6 +9,8 @@ protected:
 	void SetUp() override {
 		NOT_ON_THE_HOUR = getTime(2021, 3, 26, 9, 5);
 		ON_THE_HOUR = getTime(2021, 3, 26, 9, 0);
+
+		bookingScheduler.setSmsSender(&testableSmsSender);
 	}
 public:
 	tm getTime(int year, int mon, int day, int hour, int min) {
@@ -30,6 +32,7 @@ public:
 	const int CAPACITY_PER_HOUR = 3;
 
 	BookingScheduler bookingScheduler{ CAPACITY_PER_HOUR };
+	TestableSmsSender testableSmsSender;
 };
 
 TEST_F(BookingItem, 예약은정시에만가능하다정시가아닌경우예약불가) {
@@ -74,10 +77,8 @@ TEST_F(BookingItem, 시간대별인원제한이있다같은시간대가다르면Capacity차있어도스케
 }
 
 TEST_F(BookingItem, 예약완료시SMS는무조건발송) {
-	TestableSmsSender testableSmsSender;
 	Schedule* schedule = new Schedule{ ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER };
-	bookingScheduler.setSmsSender(&testableSmsSender);
-
+	
 	bookingScheduler.addSchedule(schedule);
 
 	EXPECT_EQ(true, testableSmsSender.isSendMethodIsCalled());
