75a0c89b15401604f09e6defe71c707aa44c7560
donghyun.yoon <ydh1305@naver.com>
2026-05-29 14:47:06 +0900

    [feature] Email이없을 때, 이메일 발송 여부 - Test #6

--- RestaurantBooking-24027449/test.cpp
+++ RestaurantBooking-24027449/test.cpp
@@ -1,6 +1,7 @@
 #include "gmock/gmock.h"
 #include "booking_scheduler.cpp"
 #include "testable_sms_sender.cpp"
+#include "testable_mail_sender.cpp"
 
 using namespace testing;
 
@@ -85,7 +86,13 @@ TEST_F(BookingItem, 예약완료시SMS는무조건발송) {
 }
 
 TEST_F(BookingItem, 이메일이없는경우에는이메일미발송) {
+	TestableMailSender testableMailSender;
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
+	bookingScheduler.setMailSender(&testableMailSender);
 
+	bookingScheduler.addSchedule(schedule);
+
+	EXPECT_EQ(0, testableMailSender.getCountSendMailMethodIsCalled());
 }
 
 TEST_F(BookingItem, 이메일이있는경우에는이메일발송) {
